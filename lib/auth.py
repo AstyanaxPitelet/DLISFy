d2c733086aa0432a08dcbaae722b739ddbfbc770
Pitelet Astyanax <astyanax.pitelet@gmail.com>
2023-04-25 23:22:11 +0200

    Supression de la classe d'authtification

--- lib/auth.py
+++
@@ -1,9 +0,0 @@
-import bcrypt
-
-class Auth():
-
-    def verify_password(passwordSasie, passwordDb):
-        return bcrypt.checkpw(passwordSasie, passwordDb)
-
-    def get_user(mail):
-        return db.users.find_one({"mail": f"{mail}"})['password']
